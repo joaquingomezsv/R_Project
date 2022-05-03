790b8a1f011e698da522e21b7db9067954e9856c
jama2250 <joaquingomezsv@gmail.com>
2022-05-02 21:55:48 -0400

    adding data sets and merged script

---
+++ R_Project_2.R
@@ -0,0 +1,323 @@
+library(tidyverse)
+library(readxl)
+library(xlsx)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+merged_data2 = read_excel('merged_index_data.xlsx')
+
+str(merged_data2)
+head(merged_data2)
+
+names(merged_data2) <- gsub(' ','.', names(merged_data2))
+
+
+
+merged_data2$Year <- as.Date(ISOdate(merged_data2$Year, 1, 1))  # beginning of year
+merged_data2['Year'] <- year(merged_data2$Year)
+
+head(merged_data2)
+
+merged_data2 = transform(merged_data2, World.Rank = as.integer(World.Rank))
+merged_data2 = transform(merged_data2, Region.Rank = as.integer(Region.Rank))
+merged_data2 = transform(merged_data2, Score = as.numeric(Score))
+merged_data2 = transform(merged_data2, Property.Rights = as.numeric(Property.Rights))
+merged_data2 = transform(merged_data2, Judical.Effectiveness = as.numeric(Judical.Effectiveness))
+merged_data2 = transform(merged_data2, Government.Integrity = as.numeric(Government.Integrity))
+merged_data2 = transform(merged_data2, Tax.Burden = as.numeric(Tax.Burden))
+merged_data2 = transform(merged_data2, Gov.t.Spending = as.numeric(Gov.t.Spending))
+merged_data2 = transform(merged_data2, Fiscal.Health = as.numeric(Fiscal.Health))
+merged_data2 = transform(merged_data2, Business.Freedom = as.numeric(Business.Freedom))
+merged_data2 = transform(merged_data2, Labor.Freedom = as.numeric(Labor.Freedom))
+merged_data2 = transform(merged_data2, Monetary.Freedom = as.numeric(Monetary.Freedom))
+merged_data2 = transform(merged_data2, Trade.Freedom = as.numeric(Trade.Freedom))
+merged_data2 = transform(merged_data2, Investment.Freedom = as.numeric(Investment.Freedom))
+merged_data2 = transform(merged_data2, Financial.Freedom = as.numeric(Financial.Freedom))
+merged_data2 = transform(merged_data2, Tariff.Rate = as.numeric(Tariff.Rate))
+merged_data2 = transform(merged_data2, Income.Tax.Rate = as.numeric(Income.Tax.Rate))
+merged_data2 = transform(merged_data2, Corporate.Tax.Rate = as.numeric(Corporate.Tax.Rate))
+merged_data2 = transform(merged_data2, Tax.Burden.of.GDP = as.numeric(Tax.Burden.of.GDP))
+merged_data2 = transform(merged_data2, Gov.t.Expenditure.of.GDP = as.numeric(Gov.t.Expenditure.of.GDP))
+merged_data2 = transform(merged_data2, Population.Millions = as.numeric(Population.Millions))
+merged_data2 = transform(merged_data2, GDP.Billions.PPP = as.numeric(GDP.Billions.PPP))
+merged_data2 = transform(merged_data2, GDP.Growth.Rate = as.numeric(GDP.Growth.Rate))
+merged_data2 = transform(merged_data2, X5.Year.GDP.Growth.Rate = as.numeric(X5.Year.GDP.Growth.Rate))
+merged_data2 = transform(merged_data2, GDP.per.Capita = as.numeric(GDP.per.Capita))
+merged_data2 = transform(merged_data2, Unemployment = as.numeric(Unemployment))
+merged_data2 = transform(merged_data2, Inflation = as.numeric(Inflation))
+merged_data2 = transform(merged_data2, FDI.Inflow = as.numeric(FDI.Inflow))
+merged_data2 = transform(merged_data2, Public.Debt = as.numeric(Public.Debt))
+
+merged_data2$Country.Name[merged_data2$Country.Name=="Hong Kong SAR"]<-"Hong Kong"
+
+str(merged_data2)
+
+merged_data2 = merged_data2 %>% drop_na()
+
+merged_data2 %>%
+  group_by(Region) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+
+merged_data2 %>%
+  group_by(Region, Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+
+merged_data2 %>%
+  group_by(Region, Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+filter(merged_data2, Region == 'Americas') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Americas') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+filter(merged_data2, Region == 'Sub-Saharan Africa') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Sub-Saharan Africa') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+filter(merged_data2, Region == 'Asia-Pacific') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Asia-Pacific') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+filter(merged_data2, Region == 'Europe') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Europe') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+
+filter(merged_data2, Region == 'Middle East and North Africa') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Middle East and North Africa') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+
+merged_data2 %>%
+  group_by(Year, Region) %>%
+  summarise(mean_score = mean(Score)) %>%
+  arrange(desc(mean_score))
+
+merged_data2 %>%
+  group_by(Year, Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(mean_score)
+
+merged_data2 %>%
+  group_by(Year, Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+
+merged_data2 %>%
+  group_by(Year, Country.Name, Region.Rank) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+
+data_2017_x = filter(merged_data2,Year=='2017')
+data_2017_x = select(data_2017, Country.Name, Region.Rank, World.Rank, Score)
+data_2022_x = filter(merged_data2,Year=='2022')
+data_2022_x = select(data_2022, Country.Name, Region.Rank, World.Rank, Score)
+
+merged_2017_2022 = inner_join(data_2017_x,data_2022_x, by='Country.Name')
+merged_2017_2022 = merged_2017_2022 %>%
+  rename(region_rank_2017 = Region.Rank.x, world_rank_2017 = World.Rank.x, score_2017 = Score.x, region_rank_2022 = Region.Rank.y,
+         world_rank_2022 = World.Rank.y, score_2022 = Score.y)
+
+merged_2017_2022['Change in World Rank'] = merged_2017_2022['world_rank_2022'] - merged_2017_2022['world_rank_2017']
+merged_2017_2022['Change in Region Rank'] = merged_2017_2022['region_rank_2022'] - merged_2017_2022['region_rank_2017']
+merged_2017_2022['Change in Score'] = merged_2017_2022['score_2022'] - merged_2017_2022['score_2017']
+merged_2017_2022
+
+merged_data2 = inner_join(merged_data2, merged_2017_2022, by='Country.Name')
+
+merged_data2
+
+
+merged_data2 %>%
+  group_by(Region) %>%
+  summarise(mean_change_in_world_rank = mean(Change.in.World.Rank))
+
+merged_data2 %>%
+  group_by(Region) %>%
+  summarise(mean_change_in_region_rank = mean(Change.in.Region.Rank))
+
+merged_data2 %>%
+  group_by(Region) %>%
+  summarise(mean_change_in_score = mean(Change.in.Score))
+
+
+merged_data2 %>%
+  group_by(Country.Name, Year) %>%
+  arrange(desc(Change.in.World.Rank))
+
+
+merged_data2 %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.Score)) %>%
+  top_n(-5, mean_change_in_score) %>%
+  arrange(mean_change_in_score)
+
+filter(merged_data2, Region == 'Americas') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.Region.Rank)) %>%
+  top_n(5, mean_change_in_score) %>%
+  arrange(desc(mean_change_in_score))
+
+filter(merged_data2, Region == 'Americas') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.Region.Rank)) %>%
+  top_n(-5, mean_change_in_score) %>%
+  arrange(mean_change_in_score)
+
+filter(merged_data2, Region == 'Sub-Saharan Africa') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.Region.Rank)) %>%
+  top_n(5, mean_change_in_score) %>%
+  arrange(desc(mean_change_in_score))
+
+filter(merged_data2, Region == 'Asia-Pacific') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.Region.Rank)) %>%
+  top_n(5, mean_change_in_score) %>%
+  arrange(mean_change_in_score)
+
+filter(merged_data2, Region == 'Asia-Pacific') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.World.Rank)) %>%
+  top_n(-5, mean_change_in_score) %>%
+  arrange(mean_change_in_score)
+
+filter(merged_data2, Region == 'Asia-Pacific') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.Score)) %>%
+  top_n(-5, mean_change_in_score) %>%
+  arrange(mean_change_in_score)
+
+filter(merged_data2, Region == 'Asia-Pacific') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Asia-Pacific') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+filter(merged_data2, Region == 'Europe') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Europe') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+
+filter(merged_data2, Region == 'Middle East and North Africa') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+filter(merged_data2, Region == 'Middle East and North Africa') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score) %>%
+  arrange(mean_score)
+
+
+merged_data2 %>%
+  group_by(Year, Region) %>%
+  summarise(mean_score = mean(Score)) %>%
+  arrange(desc(mean_score))
+
+merged_data2 %>%
+  group_by(Year, Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(mean_score)
+
+merged_data2 %>%
+  group_by(Year, Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+
+merged_data2 %>%
+  group_by(Year, Country.Name, Region.Rank) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(desc(mean_score))
+
+summary(merged_data2) #Summary of Features
+sapply(merged_data2, sd) #Standard deviations 
+
+
+model = lm(Score ~ Property.Rights + Judical.Effectiveness + Government.Integrity + Tax.Burden + Gov.t.Spending + Fiscal.Health + Business.Freedom
+           + Labor.Freedom + Monetary.Freedom + Trade.Freedom + Investment.Freedom + Financial.Freedom + Tariff.Rate 
+           + Income.Tax.Rate + Corporate.Tax.Rate + Tax.Burden.of.GDP + Gov.t.Expenditure.of.GDP + Population.Millions +
+             GDP.Billions.PPP + GDP.Growth.Rate + X5.Year.GDP.Growth.Rate + GDP.per.Capita + Unemployment + Inflation + FDI.Inflow + Public.Debt, data = merged_data2) #Use the linear model function lm() to
+#conduct the simple linear regression.
+
+summary(model)
+
+model = lm(Score ~ Tariff.Rate + Income.Tax.Rate + Corporate.Tax.Rate + Tax.Burden.of.GDP + Gov.t.Expenditure.of.GDP 
+          +GDP.Billions.PPP  + X5.Year.GDP.Growth.Rate + GDP.per.Capita + Inflation + FDI.Inflow + Public.Debt, data = merged_data2) #Use the linear model function lm() to
+#conduct the simple linear regression.
+
+summary(model)
