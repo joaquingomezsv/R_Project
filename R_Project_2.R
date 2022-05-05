1d82ca59387677eab801c6b28db891fca0b2a840
jama2250 <joaquingomezsv@gmail.com>
2022-05-04 21:22:15 -0400

    created features and added multiple linear regressions for GDP and Score

--- R_Project_2.R
+++ R_Project_2.R
@@ -42,13 +42,74 @@ merged_data2 = transform(merged_data2, FDI.Inflow = as.numeric(FDI.Inflow))
 merged_data2 = transform(merged_data2, Public.Debt = as.numeric(Public.Debt))
 merged_data2$Country.Name[merged_data2$Country.Name=="Hong Kong SAR"]<-"Hong Kong"
 merged_data2 = merged_data2 %>% drop_na()
-merged_data2 = merged_data2 %>% mutate(Quartile = ifelse(0 < Score & Score < 60 , 'Least Free',
+merged_data2 = merged_data2 %>% mutate(Quartile = ifelse(0 < Score & Score < 60 , 'Repressed',
                                            ifelse(Score < 70, 'Moderately Free', 'Most Free')))
 
+merged_data2['Mean_Rule_of_Law'] = mean(c(Property.Rights,Government.Integrity,Judical.Effectiveness))
+merged_data2['Mean_Rule_of_Law'] = (merged_data2['Property.Rights'] + merged_data2['Government.Integrity'] + merged_data2['Judical.Effectiveness'])/3
+merged_data2['Mean_Regulatory_Efficiency'] = (merged_data2['Business.Freedom'] + merged_data2['Labor.Freedom'] + merged_data2['Monetary.Freedom'])/3
+merged_data2['Mean_Government_Size'] = (merged_data2['Gov.t.Spending'] + merged_data2['Tax.Burden'] + merged_data2['Fiscal.Health'])/3
+merged_data2['Mean_Open_Markets'] = (merged_data2['Trade.Freedom'] + merged_data2['Investment.Freedom'] + merged_data2['Financial.Freedom'])/3
+Mean_Open_Markets = mean(c(Trade.Freedom,Investment.Freedom,Financial.Freedom))
+
+merged_data2
+
+merged_data2 %>%
+  group_by(Year, Region) %>%
+  summarise(Mean_Rule_of_Law = mean(c(Property.Rights,Government.Integrity,Judical.Effectiveness)))
+
+merged_data2 %>%
+  group_by(Quartile) %>%
+  ggplot() +
+  geom_boxplot(aes(Quartile, Mean_Rule_of_Law))
+
+merged_data2 %>%
+  group_by(Quartile) %>%
+  ggplot() +
+  geom_boxplot(aes(Quartile, Mean_Regulatory_Efficiency))
+
+merged_data2 %>%
+  group_by(Quartile) %>%
+  ggplot() +
+  geom_boxplot(aes(Quartile, Mean_Government_Size))
+
+
+
+merged_data2 %>%
+  group_by(Year, Region) %>%
+  ggplot() +
+  geom_point(aes(Mean_Rule_of_Law, Score, color=factor(Region))) +
+  facet_wrap(~ Year)
+
+merged_data2 %>%
+  group_by(Year, Region) %>%
+  summarise(Mean_Government_Size = mean(c(Gov.t.Spending,Tax.Burden,Fiscal.Health)))
+
+merged_data2 %>%
+  group_by(Year, Region) %>%
+  summarise(Mean_Regulatory_Efficiency = mean(c(Business.Freedom,Labor.Freedom,Monetary.Freedom)))
+
 #Data Visualization
+merged_data2 %>%
+  group_by(Quartile) %>%
+  ggplot() +
+  geom_boxplot(
+    aes(Quartile, GDP.per.Capita),
+  )
+
+names(merged_data2)
+
+model = lm(GDP.per.Capita ~ Score + Mean_Rule_of_Law + Mean_Regulatory_Efficiency + Mean_Government_Size, data = merged_data2)
+summary(model)
+
+model = lm(GDP.per.Capita ~ Score + Mean_Rule_of_Law + Mean_Regulatory_Efficiency + Mean_Government_Size, data = merged_data2)
+summary(model)
+
+
 ggplot(data = merged_data2, aes(x = GDP.per.Capita, y = Score)) +
   geom_point() + geom_smooth(method = "lm")
 
+cor(merged_data2$Score, merged_data2$GDP.Billions.PPP)
 
 ggplot(data = merged_data2, aes(x = Region, y = Score)) +
   geom_point(aes(color = Region)) 
@@ -76,6 +137,8 @@ merged_data2 %>%
   select(Country.Name, Year, GDP.Billions.PPP, Score, Region) 
 
 
+
+
 top_GDPs = merged_data2 %>%
   group_by(Country.Name, Year) %>%
   slice_max(GDP.Billions.PPP, n = 5) %>%
@@ -87,10 +150,58 @@ top_Scores = merged_data2 %>%
   group_by(Country.Name, Year) %>%
   slice_max(Score, n = 5) %>%
   arrange(desc(Score)) %>%
-  select(Country.Name, Year, GDP.Billions.PPP, Score, Region) 
+  select(Country.Name, Year, GDP.Billions.PPP, GDP.per.Capita, Score, Region) 
+
+top_Scores %>%
+  group_by(Country.Name) %>%
+  summarise(Mean_GDP_Per_Capita = mean(GDP.per.Capita)) %>%
+  top_n(5, Mean_GDP_Per_Capita) %>%
+  ggplot() + geom_col(aes(Country.Name, Mean_GDP_Per_Capita))
+
+
+top_Scores %>%
+  group_by(Country.Name) %>%
+  summarise(Mean_GDP_Per_Capita = mean(GDP.per.Capita)) %>%
+  top_n(-5, Mean_GDP_Per_Capita) %>%
+  ggplot() + geom_col(aes(Country.Name, Mean_GDP_Per_Capita))
+
+top_Scores %>%
+  group_by(Region) %>%
+  top_n(-5, GDP.per.Capita) %>%
+  distinct(Country.Name)
+
+head(top_Scores %>% group_by(Region, Year) %>% slice_max(order_by = Score, n = 5), 5)
+head(top_Scores %>% group_by(Region, Year) %>% slice_min(order_by = Score, n = 5), 5)
+
+top_Scores %>%
+  group_by(Region) %>%
+  top_n(5, GDP.per.Capita) %>%
+  distinct(Country.Name)
+
+merged_data2 %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score)
+
+merged_data2 %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score)
+
+top_Scores %>% ggplot() +
+  geom_point(aes(Mean_Rule_of_Law, Score, color=factor(Region))) +
+  facet_wrap(~ Year)
+  
+top_Scores %>%
+  group_by(Region) %>%
+  ggplot() + geom_bar(aes)
+
+ggplot(data = top_Scores %>%
+         group_by(Region), aes(x=Country.Name)) + geom_bar(aes(fill = Score))
 
 #More Data Visualization
-ggplot(data = head(top_Scores, 20)) + aes(x = GDP.Billions.PPP, y = Score) + geom_point(aes(color = Country.Name))
+ggplot(data = head(top_Scores, 40)) + aes(x = GDP.Billions.PPP, y = Score) + geom_point(aes(color = Country.Name)) +
+  facet_wrap(~ Year)
 ggplot(data = tail(top_Scores, 20)) + aes(x = GDP.Billions.PPP, y = Score) + geom_point(aes(color = Country.Name))
 
 ggplot(data = head(top_GDPs, 50)) + aes(x = GDP.Billions.PPP, y = Score) + geom_point(aes(color = Country.Name))
@@ -112,12 +223,33 @@ ggplot(data=merged_data2 %>%
   geom_bar(aes(x=Region,fill=mean_score), position = 'dodge')
 
 #More Data Manipulation
-emerged_data2 %>%
+merged_data2 %>%
   group_by(Region, Country.Name) %>%
   summarise(mean_score = mean(Score)) %>%
   top_n(5, mean_score) %>%
   arrange(desc(mean_score))
 
+merged_data2 %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score)
+
+ggplot(data=merged_data2 %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(5, mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
+merged_data2 %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(-5, mean_score)
+
+ggplot(data=merged_data2 %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(-5, mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
 
 merged_data2 %>%
   group_by(Region, Country.Name) %>%
@@ -126,10 +258,13 @@ merged_data2 %>%
   arrange(mean_score)
 
 filter(merged_data2, Region == 'Americas') %>%
-  group_by(Country.Name) %>%
+  group_by(Country.Name, Year) %>%
   summarise(mean_score = mean(Score)) %>%
-  top_n(5, mean_score) %>%
-  arrange(desc(mean_score))
+  top_n(2, mean_score) %>%
+  arrange(desc(mean_score))  %>%
+  ggplot() +
+  geom_point(aes(mean_score, Country.Name, color=factor(Country.Name))) +
+  facet_wrap(~ Year)
 
 filter(merged_data2, Region == 'Americas') %>%
   group_by(Country.Name) %>%
@@ -137,17 +272,53 @@ filter(merged_data2, Region == 'Americas') %>%
   top_n(-5, mean_score) %>%
   arrange(mean_score)
 
+filter(merged_data2, Region == 'Americas') %>%
+  group_by(Country.Name) %>%
+  summarise(mean_score = mean(Score)) %>%
+  top_n(5, mean_score) %>%
+  arrange(mean_score)
+
+ggplot(data=filter(merged_data2, Region == 'Americas') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
+ggplot(data=filter(merged_data2, Region == 'Americas') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(-5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
 filter(merged_data2, Region == 'Sub-Saharan Africa') %>%
   group_by(Country.Name) %>%
   summarise(mean_score = mean(Score)) %>%
   top_n(5, mean_score) %>%
   arrange(desc(mean_score))
 
+ggplot(data=filter(merged_data2, Region == 'Sub-Saharan Africa') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
+
 filter(merged_data2, Region == 'Sub-Saharan Africa') %>%
   group_by(Country.Name) %>%
   summarise(mean_score = mean(Score)) %>%
   top_n(-5, mean_score) %>%
   arrange(mean_score)
+
+ggplot(data=filter(merged_data2, Region == 'Sub-Saharan Africa') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(-5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
 
 filter(merged_data2, Region == 'Asia-Pacific') %>%
   group_by(Country.Name) %>%
@@ -155,17 +326,41 @@ filter(merged_data2, Region == 'Asia-Pacific') %>%
   top_n(5, mean_score) %>%
   arrange(desc(mean_score))
 
+ggplot(data=filter(merged_data2, Region == 'Asia-Pacific') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
+
 filter(merged_data2, Region == 'Asia-Pacific') %>%
   group_by(Country.Name) %>%
   summarise(mean_score = mean(Score)) %>%
   top_n(-5, mean_score) %>%
   arrange(mean_score)
 
+ggplot(data=filter(merged_data2, Region == 'Asia-Pacific') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(-5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
+
 filter(merged_data2, Region == 'Europe') %>%
   group_by(Country.Name) %>%
   summarise(mean_score = mean(Score)) %>%
   top_n(5, mean_score) %>%
   arrange(desc(mean_score))
+
+ggplot(data=filter(merged_data2, Region == 'Europe') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
 
 filter(merged_data2, Region == 'Europe') %>%
   group_by(Country.Name) %>%
@@ -173,6 +368,12 @@ filter(merged_data2, Region == 'Europe') %>%
   top_n(-5, mean_score) %>%
   arrange(mean_score)
 
+ggplot(data=filter(merged_data2, Region == 'Europe') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(-5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
 
 filter(merged_data2, Region == 'Middle East and North Africa') %>%
   group_by(Country.Name) %>%
@@ -180,17 +381,33 @@ filter(merged_data2, Region == 'Middle East and North Africa') %>%
   top_n(5, mean_score) %>%
   arrange(desc(mean_score))
 
+ggplot(data=filter(merged_data2, Region == 'Middle East and North Africa') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
+
+
 filter(merged_data2, Region == 'Middle East and North Africa') %>%
   group_by(Country.Name) %>%
   summarise(mean_score = mean(Score)) %>%
   top_n(-5, mean_score) %>%
   arrange(mean_score)
 
+ggplot(data=filter(merged_data2, Region == 'Middle East and North Africa') %>%
+         group_by(Country.Name) %>%
+         summarise(mean_score = mean(Score)) %>%
+         top_n(-5, mean_score) %>%
+         arrange(mean_score), aes(x=reorder(Country.Name, -mean_score), y=mean_score)) +
+  geom_bar(stat='identity', aes(fill=factor(mean_score))) + xlab('Country') + ylab('Mean Score') + theme(legend.position = "none")
 
 merged_data2 %>%
   group_by(Year, Region) %>%
   summarise(mean_score = mean(Score)) %>%
-  arrange(desc(mean_score))
+  arrange(desc(mean_score)) %>%
+  ggplot()+ geom_col(aes(Region, mean_score, color= factor(Region))) + facet_wrap(~ Year)
+
 
 merged_data2 %>%
   group_by(Year, Country.Name) %>%
@@ -222,9 +439,9 @@ merged_2017_2022 = merged_2017_2022 %>%
   rename(region_rank_2017 = Region.Rank.x, world_rank_2017 = World.Rank.x, score_2017 = Score.x, region_rank_2022 = Region.Rank.y,
          world_rank_2022 = World.Rank.y, score_2022 = Score.y)
 
-merged_2017_2022['Change in World Rank'] = merged_2017_2022['world_rank_2022'] - merged_2017_2022['world_rank_2017']
-merged_2017_2022['Change in Region Rank'] = merged_2017_2022['region_rank_2022'] - merged_2017_2022['region_rank_2017']
-merged_2017_2022['Change in Score'] = merged_2017_2022['score_2022'] - merged_2017_2022['score_2017']
+merged_2017_2022['Change.in.World.Rank'] = merged_2017_2022['world_rank_2022'] - merged_2017_2022['world_rank_2017']
+merged_2017_2022['Change.in.Region.Rank'] = merged_2017_2022['region_rank_2022'] - merged_2017_2022['region_rank_2017']
+merged_2017_2022['Change.in.Score'] = merged_2017_2022['score_2022'] - merged_2017_2022['score_2017']
 
 
 merged_data2 = inner_join(merged_data2, merged_2017_2022, by='Country.Name')
@@ -248,6 +465,11 @@ merged_data2 %>%
   group_by(Country.Name, Year) %>%
   arrange(desc(Change.in.World.Rank))
 
+merged_data2 %>%
+  group_by(Country.Name) %>%
+  summarise(mean_change_in_score = mean(Change.in.Score)) %>%
+  top_n(5, mean_change_in_score) %>%
+  arrange(mean_change_in_score)
 
 merged_data2 %>%
   group_by(Country.Name) %>%
@@ -259,7 +481,7 @@ filter(merged_data2, Region == 'Americas') %>%
   group_by(Country.Name) %>%
   summarise(mean_change_in_score = mean(Change.in.Region.Rank)) %>%
   top_n(5, mean_change_in_score) %>%
-  arrange(desc(mean_change_in_score))
+  arrange(desc(mean_change_in_score)) 
 
 filter(merged_data2, Region == 'Americas') %>%
   group_by(Country.Name) %>%
@@ -398,23 +620,25 @@ summary(merged_data2)
 sapply(merged_data2, sd) 
 
 #Linear Regression
-model = lm(Score ~ Property.Rights + Judical.Effectiveness + Government.Integrity + Tax.Burden + Gov.t.Spending + Fiscal.Health + Business.Freedom
-           + Labor.Freedom + Monetary.Freedom + Trade.Freedom + Investment.Freedom + Financial.Freedom + Tariff.Rate 
-           + Income.Tax.Rate + Corporate.Tax.Rate + Tax.Burden.of.GDP + Gov.t.Expenditure.of.GDP + Population.Millions +
-             GDP.Billions.PPP + GDP.Growth.Rate + X5.Year.GDP.Growth.Rate + GDP.per.Capita + Unemployment + Inflation + FDI.Inflow + Public.Debt, data = merged_data2)
+model = lm(Score ~ X5.Year.GDP.Growth.Rate + GDP.per.Capita + Public.Debt, data = merged_data2)
 
 AIC(model)
+vif(model)
 
 summary(model)
 
-model = lm(Score ~ Tariff.Rate + Income.Tax.Rate + Corporate.Tax.Rate + Tax.Burden.of.GDP + Gov.t.Expenditure.of.GDP 
-          +GDP.Billions.PPP  + X5.Year.GDP.Growth.Rate + GDP.per.Capita + Inflation + FDI.Inflow + Public.Debt, data = merged_data2) 
+library(car)
+
+
+model = lm(Score ~ Tariff.Rate + Corporate.Tax.Rate + Tax.Burden.of.GDP
+           + X5.Year.GDP.Growth.Rate + GDP.per.Capita + Inflation + Public.Debt, data = merged_data2) 
 
 
 AIC(model)
 BIC(model)
 
 summary(model)
+vif(model)
 
 model = lm(GDP.Billions.PPP ~ Property.Rights + Judical.Effectiveness + Government.Integrity + Tax.Burden + Gov.t.Spending + Fiscal.Health + Business.Freedom
            + Labor.Freedom + Monetary.Freedom + Investment.Freedom + Financial.Freedom + Tariff.Rate 
@@ -422,3 +646,26 @@ model = lm(GDP.Billions.PPP ~ Property.Rights + Judical.Effectiveness + Governme
              Score + FDI.Inflow + Public.Debt, data = merged_data2) #
 
 summary(model)
+
+model = lm(GDP.per.Capita ~ Score + Mean_Regulatory_Efficiency + Mean_Government_Size + Mean_Open_Markets, data = merged_data2)
+summary(model)
+
+names(merged_data2)
+
+model = lm(World.Rank ~ Property.Rights + Judical.Effectiveness + Government.Integrity + Tax.Burden + Gov.t.Spending +
+             Fiscal.Health + Business.Freedom + Labor.Freedom + Monetary.Freedom + Trade.Freedom + 
+             Investment.Freedom + Financial.Freedom, data = merged_data2)
+
+summary(model)
+
+library(broom)
+coefs <- tidy(model)
+coefs[order(coefs$estimate, decreasing = FALSE),]
+
+coefs
+
+ggplot(data = merged_data2, aes(x = Property.Rights, y = Score)) +
+  geom_point()
+
+
+
